2c19d7a07079c73201d61217de773e0cb49919ed
morix1500 <rade1500@gmail.com>
2017-12-25 13:36:27 +0900

    complete 3

--- 3/3-6/main.go
+++ 3/3-6/main.go
@@ -15,7 +15,12 @@ var (
 func main() {
 	var stream io.Reader
 
-	// ここに各書く
+	strA := io.NewSectionReader(programing, 5, 1)
+	strS := io.NewSectionReader(system, 0, 1)
+	strC := io.NewSectionReader(computer, 0, 1)
+	strI := io.NewSectionReader(programing, 8, 1)
+	strI2 := io.NewSectionReader(programing, 8, 1)
+	stream = io.MultiReader(strA, strS, strC, strI, strI2)
 
 	io.Copy(os.Stdout, stream)
 }
