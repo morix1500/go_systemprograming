ff399b928ad720a8bb805dd11a3982f34c42c1a4
Morix <rade1500@gmail.com>
2018-01-17 04:57:56 +0000

    12 start

---
+++ 12/server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "github.com/lestrrat/go-server-starter/listener"
+    "net/http"
+    "os"
+    "os/signal"
+    "syscall"
+)
+
+func main() {
+    signals := make(chan os.Signal, 1)
+    signal.Notify(signals, syscall.SIGTERM)
+
+    listeners, err := listener.ListenAll()
+    if err != nil {
+        panic(err)
+    }
+    server := http.Server{
+        Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+            fmt.Fprintf(w, "server pid: %d %v\n", os.Getpid(), os.Environ())
+        }),
+    }
+    go server.Serve(listeners[0])
+    <-signals
+    server.Shutdown(context.Background())
+}
